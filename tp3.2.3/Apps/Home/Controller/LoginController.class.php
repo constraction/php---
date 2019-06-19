fc34b76fd56da7a69985c4ab973a10257059f039
东檑 李 <845909161@qq.com>
2019-06-19 10:30:27 +0800

    1

--- tp3.2.3/Apps/Home/Controller/LoginController.class.php
+++ tp3.2.3/Apps/Home/Controller/LoginController.class.php
@@ -403,5 +403,9 @@ class LoginController extends Controller {
 			echo "查询失败！".mysql_error();			
 		}
 		$this->display("./Login_select()");
+
+
+		ini_set('display_errors','on');
+		error_reporting(E_ALL);
     }
 }
