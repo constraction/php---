ad2ff234126e55beefd48c844210503cf3e76f28
东檑 李 <845909161@qq.com>
2019-06-30 12:46:00 +0800

    1

--- tp3/Apps/Home/Controller/InformController.class.php
+++ tp3/Apps/Home/Controller/InformController.class.php
@@ -2,6 +2,7 @@
 namespace Home\Controller;
 use Think\Controller;
 use Home\Model\InformModel;
+use Think\Model\RelationModel;
 class InformController extends Controller 
 {
     public function index()
@@ -17,35 +18,7 @@ class InformController extends Controller
     }
     public function insert_insert()
     {
-        header("Content-type:text/html;charset=utf-8");
-
-        $name=I('post.name');
-        $sex=I('post.sex');
-        $grade=I('post.grade');
-        $class=I('post.class');
-        $phone=I('post.phone');
-        $mail=I('post.mail');
-
-        $data['sex']=$sex;
-        $data['grade']=$grade;
-        $data['class']=$class;
-        $data['phone']=$phone;
-        $data['mail']=$mail;
-
-        $where=array(
-            'user.name'=>$name
-        );
-        $rs=M('student') 
-            ->join("user on user.uid=student.sid") //附表连主表
-            ->where('sid=2')
-            // ->fetchSql(true)
-            ->save($data);
-        // dump($rs);
-        
-        if ($rs) {
-            $this->success('修改成功', '../Inform/index', 1);
-        } else {
-            $this->error('修改失败');
-        } 
+        $inform=new InformModel();
+        $inform->insert();
     }
 }
--- tp3/Apps/Home/Model/InformModel.class.php
+++ tp3/Apps/Home/Model/InformModel.class.php
@@ -2,9 +2,17 @@
 namespace Home\Model;
 use Think\Model;
 use Think\Controller;
-class InformModel extends Model 
+use Think\Model\RelationModel;
+class InformModel extends RelationModel 
 {
     protected $tableName='student';
+
+    protected $_link = array(
+        'User' => array(
+            'mapping_type'=>HAS_ONE,
+            'foreign_key '=>'name'
+        )
+    );
     public function index()
     {
         header("Content-type:text/html;charset=utf-8");
@@ -51,6 +59,37 @@ class InformModel extends Model
     }
     public function insert()
     {
+        header("Content-type:text/html;charset=utf-8");
+
+        $name=I('post.name');
+        $sex=I('post.sex');
+        $grade=I('post.grade');
+        $class=I('post.class');
+        $phone=I('post.phone');
+        $mail=I('post.mail');
+
+        $data['sex']=$sex;
+        $data['grade']=$grade;
+        $data['class']=$class;
+        $data['phone']=$phone;
+        $data['mail']=$mail;
+        $data['Login']=$name;
+        $where=array(
+            'user.name'=>$name
+        );
+        $rs=D('student') 
+            ->join("user on user.uid=student.sid") //附表连主表
+            ->relation(true)
+            // ->fetchSql(true)
+            ->save($data);
+        // dump($rs);
         
+        if ($rs) {
+            echo "修改成功";
+            // $this->success('修改成功', '../Inform/index', 1);
+        } else {
+            echo "修改失败";
+            // $this->error('修改失败');
+        } 
     }
 }
